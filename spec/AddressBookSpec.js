1a6d7d54d19faae43caa6c26adeedbffdcb1cdcb
Manpreet Singh Lalria <manpreet.singh.lalria@gmail.com>
2023-02-07 07:08:18 +0000

    test: add AddressBook test suite with addContact() test

---
+++ spec/AddressBookSpec.js
@@ -0,0 +1,10 @@
+describe("Address Book", () => {
+    it('should be able to add a contact', () => {
+        let addressBook = new AddressBook();
+        let thisContact = new Contact();
+
+        addressBook.addContact(thisContact);
+
+        expect(addressBook.getContact(0)).toBe(thisContact);
+    })
+});
