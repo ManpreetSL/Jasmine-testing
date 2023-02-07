c8c1d9704ddbca598247be7275314e9920b77c07
Manpreet Singh Lalria <manpreet.singh.lalria@gmail.com>
2023-02-07 09:51:12 +0000

    test: add async getContacts() test to Address Book spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,21 +1,32 @@
-describe("Address Book", () => {
-  let addressBook;
-  let contact;
+describe('Address Book', () => {
+    let addressBook
+    let contact
 
-  beforeEach(() => {
-    addressBook = new AddressBook();
-    contact = new Contact();
+    beforeEach(() => {
+        addressBook = new AddressBook()
+        contact = new Contact()
 
-    addressBook.addContact(contact);
-  });
+        addressBook.addContact(contact)
+    })
 
-  it("should be able to add a contact", () => {
-    expect(addressBook.getContact(0)).toBe(contact);
-  });
+    it('should be able to add a contact', () => {
+        expect(addressBook.getContact(0)).toBe(contact)
+    })
 
-  it("should be able to delete a contact", () => {
-    addressBook.deleteContact(0);
+    it('should be able to delete a contact', () => {
+        addressBook.deleteContact(0)
 
-    expect(addressBook.getContact(0)).not.toBeDefined();
-  });
-});
+        expect(addressBook.getContact(0)).not.toBeDefined()
+    })
+
+    describe('Address Book async', () => {
+        beforeEach((done) => {
+            addressBook = new AddressBook()
+            addressBook.getContacts(() => done())
+        })
+
+        it('should be able to load contacts initially', () => {
+            expect(addressBook.loaded).toBe(true)
+        })
+    })
+})
