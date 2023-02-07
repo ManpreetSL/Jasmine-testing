53a34d04b5233bdacffe5a4b529c352547b65196
Manpreet Singh Lalria <manpreet.singh.lalria@gmail.com>
2023-02-07 07:23:56 +0000

    test: add deleteContact() test

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,10 +1,20 @@
 describe("Address Book", () => {
-    it('should be able to add a contact', () => {
-        let addressBook = new AddressBook();
-        let thisContact = new Contact();
+  it("should be able to add a contact", () => {
+    let addressBook = new AddressBook();
+    let thisContact = new Contact();
 
-        addressBook.addContact(thisContact);
+    addressBook.addContact(thisContact);
 
-        expect(addressBook.getContact(0)).toBe(thisContact);
-    })
+    expect(addressBook.getContact(0)).toBe(thisContact);
+  });
+
+  it('should be able to delete a contact', () => {
+    let addressBook = new AddressBook();
+    let contact = new Contact();
+
+    addressBook.addContact(contact);
+    addressBook.deleteContact(0);
+
+    expect(addressBook.getContact(0)).not.toBeDefined();
+  });
 });
