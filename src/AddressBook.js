c53be21673131e0dd38b2a3f6dbe6e8800149f59
Manpreet Singh Lalria <manpreet.singh.lalria@gmail.com>
2023-02-07 09:51:32 +0000

    feat: add getContacts() implementation

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,15 +1,24 @@
-const AddressBook = function() {
-    this.contacts = [];
-}
+const AddressBook = function () {
+  this.contacts = [];
+  this.loaded = false;
+};
 
-AddressBook.prototype.addContact = function(contact) {
-    this.contacts.push(contact);
-}
+AddressBook.prototype.addContact = function (contact) {
+  this.contacts.push(contact);
+};
 
-AddressBook.prototype.getContact = function(index) {
-    return this.contacts.at(index);
-}
+AddressBook.prototype.getContact = function (index) {
+  return this.contacts.at(index);
+};
 
-AddressBook.prototype.deleteContact = function(index) {
-    this.contacts.splice(index, 1);
-}
+AddressBook.prototype.deleteContact = function (index) {
+  this.contacts.splice(index, 1);
+};
+
+AddressBook.prototype.getContacts = function (callback) {
+  setTimeout(() => {
+    this.loaded = true;
+
+    if (callback) return callback();
+  }, 1000);
+};
